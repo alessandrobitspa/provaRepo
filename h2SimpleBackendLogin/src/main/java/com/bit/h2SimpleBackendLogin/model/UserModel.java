3f179f4d56c082fc84c47eb2bfe68bdb385ea42e
luca-ruggiero-bit <luca.ruggiero@bitspa.it>
2025-11-28 15:27:25 +0100

    Aggiunta controllo username e collegamento db

--- h2SimpleBackendLogin/src/main/java/com/bit/h2SimpleBackendLogin/model/UserModel.java
+++ h2SimpleBackendLogin/src/main/java/com/bit/h2SimpleBackendLogin/model/UserModel.java
@@ -9,14 +9,14 @@ import jakarta.persistence.Id;
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
     private String username;
     private String password;
 
     // Getters & Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
--- h2SimpleBackendLogin/src/main/java/com/bit/h2SimpleBackendLogin/repo/UserRepo.java
+++ h2SimpleBackendLogin/src/main/java/com/bit/h2SimpleBackendLogin/repo/UserRepo.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
  @Repository
-public interface UserRepo extends JpaRepository<UserModel, Long> {
+public interface UserRepo extends JpaRepository<UserModel, Integer> {
     Optional<UserModel> findByUsername(String username);
     boolean existsByUsername(String username);
 }
